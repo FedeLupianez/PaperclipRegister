d310121f68361b79945ba759c4e6bd4b36a125c4
fede <lupianezfederico@gmail.com>
2025-02-09 12:26:28 -0300

    Agrego el poder seleccionar un texto copiado con enter y que se escriba

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import keyboard
 import pyperclip
 import time
+import tkinter as tk
 
 
 before_copies: list[str] = []
@@ -9,7 +10,6 @@ selection: int = 0
 
 
 def get_paperclip() -> None:
-    global record
     time.sleep(0.5) # Espero para que se copie primero en el portapapeles
     content: str = pyperclip.paste()
     
@@ -22,11 +22,14 @@ def get_paperclip() -> None:
 
 
 def show_before_copies() -> None:
+    global is_selection_activate
+    is_selection_activate = True
     print('---------------------------')
     for index in range(len(before_copies)):
         if (index == selection): 
             print('@', end='')
         print(before_copies[index])
+        
 
 def move_selection_down() -> None:
     global selection
@@ -34,23 +37,28 @@ def move_selection_down() -> None:
     if (selection > 5):
         selection = 0
 
-    print("Seleccion : ", selection)
 
 def move_selection_up() -> None:
     global selection
     selection -= 1
     if (selection < 0):
         selection = 5
-        
-    print("Seleccion : ", selection)
-    
 
-    
+
+def paste_selection() -> None:
+    global is_selection_activate
+    if (not is_selection_activate):
+        return
+
+    keyboard.write(before_copies[selection])
+    is_selection_activate = False
+
 
 keyboard.add_hotkey("ctrl+c", get_paperclip)
 keyboard.add_hotkey("ctrl+alt+v", show_before_copies)
 keyboard.add_hotkey("down", move_selection_down)
 keyboard.add_hotkey("up", move_selection_up)
+keyboard.add_hotkey("enter", paste_selection)
 
 
 keyboard.wait('esc')
